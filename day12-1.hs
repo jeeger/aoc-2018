41403728e43be2ed82f88277b6932289051e0dbf
Jan Seeger <jan.seeger@siemens.com>
2018-12-18 16:48:36 +0100

    Added day 12/1.

--- day12-1.hs
+++ day12-1.hs
@@ -3,19 +3,31 @@ module Main where
 import Prelude hiding (putStr,getContents)
 
 import Data.Attoparsec.Text(Parser, decimal, char, space, endOfLine, many1, parseOnly, anyChar, manyTill, (<?>))
-import Data.Text
+import qualified Data.Text as T
 import Data.Text.IO
 import Data.Functor(($>))
+import Text.Pretty.Simple(pPrint)
 import Control.Applicative((<|>))
+import qualified Data.HashMap.Strict as M
 import qualified Formatting as F
+
+import Debug.Trace(traceShowId)
 
 
 data PlantState = Plant
-                | NoPlant deriving Show
+                | NoPlant deriving (Show, Eq)
 
-data Rule = Rule [PlantState] PlantState deriving Show
+data Rule = Rule { rCond:: [PlantState]
+                 , rResult:: PlantState
+                 } deriving Show
+
 newtype Rules = Rules [Rule] deriving Show
-newtype Plants = Plants [PlantState] deriving Show
+newtype Plants = Plants { pPlants:: M.HashMap Int PlantState} deriving Show
+
+
+
+getPlantState:: Plants -> Int -> PlantState
+getPlantState (Plants p) idx = M.lookupDefault NoPlant idx p
 
 parsePlantState:: Parser PlantState
 parsePlantState =
@@ -35,7 +47,7 @@ parseInitialState:: Parser Plants
 parseInitialState = do
   "initial state: "
   plants <- manyTill parsePlantState endOfLine
-  return $ Plants  plants
+  return $ Plants $ M.fromList $ filter ((== Plant) . snd) $ zip [0..length plants] plants
   <?> "parseInitialState"
 
 parseAll:: Parser (Rules, Plants)
@@ -46,12 +58,39 @@ parseAll = do
   return (Rules rules, plants)
   <?> "parseAll"
 
+neighborhood:: Plants -> Int -> [PlantState]
+neighborhood p key = map (getPlantState p) [key-2..key+2]
+
+keysInRange:: Plants -> [Int]
+keysInRange (Plants p) = [(mink-3)..(maxk+3)] where
+  mink = minimum $ M.keys p
+  maxk = maximum $ M.keys p
+
+applyRule:: Rule -> Plants -> Plants
+applyRule (Rule cond rresult) p = Plants $ applyRuleToKeys (keysInRange p) M.empty where
+  applyRuleToKeys [] result = result
+  applyRuleToKeys (k:ks) result = if neighborhood p k == cond && rresult == Plant then
+                                    applyRuleToKeys ks (M.insert k Plant result)
+                                  else
+                                    applyRuleToKeys ks result
+
+emptyPlant:: Plants
+emptyPlant = Plants M.empty
+
+plantUnion:: Plants -> Plants -> Plants
+plantUnion (Plants p1) (Plants p2) = Plants $ M.unionWith (\_ _ -> error "Conflicting growths!") p1 p2
+
+grow:: Rules -> Plants -> Plants
+grow (Rules rs) p = foldr ruleUnion emptyPlant rs where
+  ruleUnion r m = applyRule r p `plantUnion` m
+
+value:: Plants -> Int
+value (Plants p) = sum (M.keys p)
+
 main:: IO ()
 main = do
   lines <- getContents
   case parseOnly parseAll lines of
     Left err -> print err
-    Right (r, p) -> do
-      print r
-      print p
-    
+    Right (r, p) ->
+      print $ value $ iterate (grow r) p !! 20
